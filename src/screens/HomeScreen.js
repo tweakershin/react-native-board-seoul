4350d79ae2dd7d390f8359cf4d509470c5bd9e86
신윤수 <us621011@gmail.com>
2019-09-26 08:38:54 +0900

    HomeScreen board변수 추가.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
+import BoardList from "../components/board/BoardList";
+
+let board = [
+  {
+    name: "샘플 게시글1",
+    content: "샘플 게시글1 내용",
+    key: "1"
+  },
+  {
+    name: "샘플 게시글2",
+    content: "샘플 게시글2 내용",
+    key: "2"
+  }
+];
+
 export default class HomeScreen extends Component {
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Text> 홈스크린! </Text>
+        <BoardList board={board} />
       </View>
     );
   }
